f64f1d4f237bc176fa52e8b2c7ea1a4f6b99848e
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-05-16 18:42:49 +0500

    created JavaScript in script.js for menu effects

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// This is for JS 
+//This is for JS
+let menu = document.getElementsByClassName("menu")[0]; // Access the first element from the collection
+
+if (window.innerWidth >= 768) {
+    menu.style.background = "none";
+    menu.style.boxShadow = "none";
+} else {
+    menu.style.backgroundColor = "var(--color-white)";
+    menu.style.boxShadow = "0 -8px 0 0 var(--color-white), 0 8px 0 0 var(--color-white)";
+}
