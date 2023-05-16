420ff0865dd0aed9ba34fe35f43e37c84e793b58
lily4178993 <nellytelli@gmail.com>
2023-05-16 22:39:47 +0000

    Redefine the function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-const menuIcon = document.getElementById('menu');
+var menuIcon = document.getElementById('menu');
 
 window.addEventListener('resize', () => {
-  if (window.innerWidth >= 768) {
-    menuIcon.style.background = 'none';
-    menuIcon.style.boxShadow = 'none';
+  if (window.innerWidth < 768) {
+    menuIcon.style.display = 'block';
   } else {
-    menuIcon.style.background = 'var(--color-white)';
-    menuIcon.style.boxShadow = '0 -8px 0 0 var(--color-white), 0 8px 0 0 var(--color-white)';
+    menuIcon.style.display = 'none';
   }
 });
