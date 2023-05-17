05367ade924ad41d76a0eb5526a261f75e8f6185
lily4178993 <nellytelli@gmail.com>
2023-05-17 14:15:16 +0000

    Removed one object elemnt from the project'array in JS file

--- js/modal-view.js
+++ js/modal-view.js
@@ -1,16 +1,5 @@
 let collectionOfProjects = [
     {
-        name: `Multi-Post Stories`,
-        description: `A daily selection of privately personalized reads; no accounts
-        or sign-ups required. has been the industry's standard dummy text
-        ever since the 1500s, when an unknown printer took a standard dummy
-        text.`,
-        featured_Image: `images/pro-img.png`,
-        technologies: ["CSS", "HTML", "Bootstrap", "Ruby"],
-        link_to_live_version: `lily4178993.github.io/Portfolio/`,
-        link_to_source_code: `https://github.com/lily4178993/Portfolio`
-    },
-    {
         name: `Profesional Art Printing Data`,
         description: `A daily selection of privately personalized reads; no accounts
         or sign-ups required. has been the industry's standard.`,
@@ -64,20 +53,23 @@ let collectionOfProjects = [
         link_to_source_code: `https://github.com/lily4178993/Portfolio`
     }
 ];
-let worksContainer = document.querySelector('works-container');
-collectionOfProjects.map((card) => {
+let worksContainer = document.querySelector('.works-container');
+console.log(worksContainer);
+collectionOfProjects.map((project) => {
     worksContainer.innerHTML += `
+    <!-- WORK BOX (begin) -->
     <div class="work-box w-disable">
-    <div class="work-content">
-    <h3 class="work-title">${card.name}</h3>
-    <p class="work-description">${card.description}</p>
-    <ul class="work-languages-list">
-    <li class="work-language">${card.technologies[0]}</li>
-    <li class="work-language">${card.technologies[1]}</li>
-    <li class="work-language">${card.technologies[2]}</li>
-    </ul>
-    <a href="#" class="btn see-work">See project</a>
+        <div class="work-content">
+            <h3 class="work-title">${project.name}</h3>
+            <p class="work-description">${project.description}</p>
+            <ul class="work-languages-list">
+                <li class="work-language">${project.technologies[0]}</li>
+                <li class="work-language">${project.technologies[1]}</li>
+                <li class="work-language">${project.technologies[2]}</li>
+            </ul>
+            <a href="#" class="btn see-work">See project</a>
+        </div>
     </div>
-    </div>
+    <!-- WORK BOX (end) -->
     `
 })
