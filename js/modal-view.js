2fe45bf6dea0000ab6cea8a9ad78e92426ff5cb2
lily4178993 <nellytelli@gmail.com>
2023-05-17 10:24:45 +0000

    Create a JavaScript array to store all of the information for all projects

---
+++ js/modal-view.js
@@ -0,0 +1,71 @@
+let collectionOfProjects = [
+    {
+        name: `Multi-Post Stories`,
+        description: `A daily selection of privately personalized reads; no accounts
+        or sign-ups required. has been the industry's standard dummy text
+        ever since the 1500s, when an unknown printer took a standard dummy
+        text.`,
+        featured_Image: `images/pro-img.png`,
+        technologies: ["CSS", "HTML", "Bootstrap", "Ruby"],
+        link_to_live_version: `lily4178993.github.io/Portfolio/`,
+        link_to_source_code: `https://github.com/lily4178993/Portfolio`
+    },
+    {
+        name: `Profesional Art Printing Data`,
+        description: `A daily selection of privately personalized reads; no accounts
+        or sign-ups required. has been the industry's standard.`,
+        featured_Image: ``,
+        technologies: ["HTML", "Bootstrap", "Ruby"],
+        link_to_live_version: `lily4178993.github.io/Portfolio/`,
+        link_to_source_code: `https://github.com/lily4178993/Portfolio`
+    },
+    {
+        name: `Data Dashboard Healthcare`,
+        description: `A daily selection of privately personalized reads; no accounts
+        or sign-ups required. has been the industry's standard.`,
+        featured_Image: ``,
+        technologies: ["HTML", "Bootstrap", "Ruby"],
+        link_to_live_version: `lily4178993.github.io/Portfolio/`,
+        link_to_source_code: `https://github.com/lily4178993/Portfolio`
+    },
+    {
+        name: `Website Portfolio`,
+        description: `A daily selection of privately personalized reads; no accounts
+        or sign-ups required. has been the industry's standard.`,
+        featured_Image: ``,
+        technologies: ["HTML", "Bootstrap", "Ruby"],
+        link_to_live_version: `lily4178993.github.io/Portfolio/`,
+        link_to_source_code: `https://github.com/lily4178993/Portfolio`
+    },
+    {
+        name: `Profesional Art Printing Data`,
+        description: `A daily selection of privately personalized reads; no accounts
+        or sign-ups required. has been the industry's standard.`,
+        featured_Image: ``,
+        technologies: ["HTML", "Bootstrap", "Ruby"],
+        link_to_live_version: `lily4178993.github.io/Portfolio/`,
+        link_to_source_code: `https://github.com/lily4178993/Portfolio`
+    },
+    {
+        name: `Data Dashboard Healthcare`,
+        description: `A daily selection of privately personalized reads; no accounts
+        or sign-ups required. has been the industry's standard.`,
+        featured_Image: ``,
+        technologies: ["HTML", "Bootstrap", "Ruby"],
+        link_to_live_version: `lily4178993.github.io/Portfolio/`,
+        link_to_source_code: `https://github.com/lily4178993/Portfolio`
+    },
+    {
+        name: `Website Portfolio`,
+        description: `A daily selection of privately personalized reads; no accounts
+        or sign-ups required. has been the industry's standard.`,
+        featured_Image: ``,
+        technologies: ["HTML", "Bootstrap", "Ruby"],
+        link_to_live_version: `lily4178993.github.io/Portfolio/`,
+        link_to_source_code: `https://github.com/lily4178993/Portfolio`
+    }
+];
+
+collectionOfProjects.forEach(project => {
+    console.log("Informations of each project: ", project);
+});
