93cd93f3bcb7587e75ea4326f139f2dea31b2403
lily4178993 <nellytelli@gmail.com>
2023-05-18 21:30:25 +0000

    Fix JS linters errors

--- js/modal-view.js
+++ js/modal-view.js
@@ -1,62 +1,62 @@
-let collectionOfProjects = [
-    {
-        name: `Profesional Art Printing Data`,
-        description: `A daily selection of privately personalized reads; no accounts
+const collectionOfProjects = [
+  {
+    name: 'Profesional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts
         or sign-ups required. has been the industry's standard.`,
-        featured_Image: `./images/pro-img.png`,
-        technologies: ["HTML", "Bootstrap", "Ruby", "Test"],
-        link_to_live_version: `lily4178993.github.io/Portfolio/`,
-        link_to_source_code: `https://github.com/lily4178993/Portfolio`
-    },
-    {
-        name: `Data Dashboard Healthcare`,
-        description: `A daily selection of privately personalized reads; no accounts
+    featured_Image: './images/pro-img.png',
+    technologies: ['HTML', 'Bootstrap', 'Ruby', 'Test'],
+    link_to_live_version: 'lily4178993.github.io/Portfolio/',
+    link_to_source_code: 'https://github.com/lily4178993/Portfolio',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: `A daily selection of privately personalized reads; no accounts
         or sign-ups required. has been the industry's standard.`,
-        featured_Image: ``,
-        technologies: ["HTML", "Bootstrap", "Ruby"],
-        link_to_live_version: `lily4178993.github.io/Portfolio/`,
-        link_to_source_code: `https://github.com/lily4178993/Portfolio`
-    },
-    {
-        name: `Website Portfolio`,
-        description: `A daily selection of privately personalized reads; no accounts
+    featured_Image: '',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link_to_live_version: 'lily4178993.github.io/Portfolio/',
+    link_to_source_code: 'https://github.com/lily4178993/Portfolio',
+  },
+  {
+    name: 'Website Portfolio',
+    description: `A daily selection of privately personalized reads; no accounts
         or sign-ups required. has been the industry's standard.`,
-        featured_Image: ``,
-        technologies: ["HTML", "Bootstrap", "Ruby"],
-        link_to_live_version: `lily4178993.github.io/Portfolio/`,
-        link_to_source_code: `https://github.com/lily4178993/Portfolio`
-    },
-    {
-        name: `Profesional Art Printing Data`,
-        description: `A daily selection of privately personalized reads; no accounts
+    featured_Image: '',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link_to_live_version: 'lily4178993.github.io/Portfolio/',
+    link_to_source_code: 'https://github.com/lily4178993/Portfolio',
+  },
+  {
+    name: 'Profesional Art Printing Data',
+    description: `A daily selection of privately personalized reads; no accounts
         or sign-ups required. has been the industry's standard.`,
-        featured_Image: ``,
-        technologies: ["HTML", "Bootstrap", "Ruby"],
-        link_to_live_version: `lily4178993.github.io/Portfolio/`,
-        link_to_source_code: `https://github.com/lily4178993/Portfolio`
-    },
-    {
-        name: `Data Dashboard Healthcare`,
-        description: `A daily selection of privately personalized reads; no accounts
+    featured_Image: '',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link_to_live_version: 'lily4178993.github.io/Portfolio/',
+    link_to_source_code: 'https://github.com/lily4178993/Portfolio',
+  },
+  {
+    name: 'Data Dashboard Healthcare',
+    description: `A daily selection of privately personalized reads; no accounts
         or sign-ups required. has been the industry's standard.`,
-        featured_Image: ``,
-        technologies: ["HTML", "Bootstrap", "Ruby"],
-        link_to_live_version: `lily4178993.github.io/Portfolio/`,
-        link_to_source_code: `https://github.com/lily4178993/Portfolio`
-    },
-    {
-        name: `Website Portfolio`,
-        description: `A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.`,
-        featured_Image: ``,
-        technologies: ["HTML", "Bootstrap", "Ruby"],
-        link_to_live_version: `lily4178993.github.io/Portfolio/`,
-        link_to_source_code: `https://github.com/lily4178993/Portfolio`
-    }
+    featured_Image: '',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link_to_live_version: 'lily4178993.github.io/Portfolio/',
+    link_to_source_code: 'https://github.com/lily4178993/Portfolio',
+  },
+  {
+    name: 'Website Portfolio',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard.',
+    featured_Image: '',
+    technologies: ['HTML', 'Bootstrap', 'Ruby'],
+    link_to_live_version: 'lily4178993.github.io/Portfolio/',
+    link_to_source_code: 'https://github.com/lily4178993/Portfolio',
+  },
 ];
-let worksContainer = document.querySelector('.works-container');
+const worksContainer = document.querySelector('.works-container');
 
 collectionOfProjects.map((project) => {
-    worksContainer.innerHTML += `
+  worksContainer.innerHTML += `
     <!-- WORK BOX (begin) -->
     <div class="work-box w-disable">
         <div class="work-content">
@@ -112,39 +112,41 @@ collectionOfProjects.map((project) => {
         </div>
     </div>
     <!-- The Modal (end)-->
-    `
+    `;
+  return ('');
 });
 
 /**
- * The following lines of code were taken from the W3School website. 
+ * The following lines of code were taken from the W3School website.
  * You can see the source by following this link:
  * https://www.w3schools.com/howto/howto_css_modals.asp
 */
 
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("openModal");
+const btn = document.getElementsByClassName('openModal');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
+// eslint-disable-next-line no-plusplus
 for (let i = 0; i < btn.length; i++) {
-    btn[i].onclick = function() {
-        modal.style.display = "block";
-    }
+  btn[i].onclick = () => {
+    modal.style.display = 'block';
+  };
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+span.onclick = () => {
+  modal.style.display = 'none';
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
   }
-}
+};
