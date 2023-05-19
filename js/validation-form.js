7ecfa05891243c1f05cc848dbf1b63f4e56da277
lily4178993 <nellytelli@gmail.com>
2023-05-19 20:00:42 +0000

    Redefine the validation function

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,22 +1,31 @@
-const checkCase = (str) => {
-  const isLower = str.toString().toLowerCase();
-  if (str === isLower) {
-    return true;
+/**
+ * This script is for the contact validation form
+ * It check the email and return if it write in lower case
+*/
+
+// Get the form
+const form = document.querySelector('#form');
+
+// Get the email
+const email = document.getElementById('email');
+
+// Get the element that will display the message error
+const emailError = document.getElementById('error');
+
+/**
+ * When user click on 'submit' button
+ * Take the value inside the email field
+ * Check if there is any special character and remove it
+ * Iterate through the value and check if there is any capital letter
+ * Return an error message if true and send the content of the form if not
+*/
+form.addEventListener('submit', (event) => {
+  const emailValue = email.value;
+  const emailchecker = emailValue.replace(/[^a-zA-Z]/g, '').split('');
+  for (let counter = 0; counter < emailchecker.length; counter += 1) {
+    if (emailchecker[counter] === emailchecker[counter].toUpperCase()) {
+      emailError.innerText = 'Oops! There is something wrong. Your email must be in lowerCase';
+      event.preventDefault();
+    }
   }
-  return false;
-};
-const validate = (email) => {
-  email.preventDefault();
-  const inputMail = document.getElementById('email').value;
-  const form = document.getElementById('form');
-  const checkEmail = checkCase(inputMail);
-  if (checkEmail === true) {
-    form.action = 'https://formspree.io/f/xvonlboj';
-    form.submit();
-  } else {
-    const throwError = document.getElementById('error');
-    throwError.innerHTML = 'Please check your email address again. The email input should always be in the lower case.';
-  }
-};
-const form = document.getElementById('form');
-form.addEventListener('submit', validate);
+});
