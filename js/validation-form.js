49b224f6b3276d9ea1a1add52892405525c3cb68
Mike111222 <Tipu870@gmail.com>
2023-05-19 18:25:26 +0500

    Update validation-form.js with form validation code

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,24 +1,22 @@
-let form = document.querySelector('form');
-let email = document.getElementById('email');
-let emailError = document.getElementById('error');
-let submitButton = document.getElementById('submit');
-
-function hasUpperCase(str) {
-    return str.toUpperCase() == str;
-}
-
-console.log(hasUpperCase(email.value));
-
-form.addEventListener('submit', () => {
-    if (email.validity.valid) {
-        if (!hasUpperCase(email.value)) {
-            form.action = 'https://formspree.io/f/xvonlboj';
-            form.submit();
-        }
-        
-    } else {
-        emailError.style.display = 'block';
-        emailError.innerHTML = 'Oops! There is something wrong. Your email must be written in lower case'
-    }
-    
-})
+const checkCase = (str) => {
+  const isLower = str.toString().toLowerCase();
+  if (str === isLower) {
+    return true;
+  }
+  return false;
+};
+const validate = (email) => {
+  email.preventDefault();
+  const inputMail = document.getElementById('email').value;
+  const form = document.getElementById('form');
+  const checkEmail = checkCase(inputMail);
+  if (checkEmail === true) {
+    form.action = 'https://formspree.io/f/xvonlboj';
+    form.submit();
+  } else {
+    const throwError = document.getElementById('error');
+    throwError.innerHTML = 'Please check your email address again. The email input should always be in the lower case.';
+  }
+};
+const form = document.getElementById('form');
+form.addEventListener('submit', validate);
